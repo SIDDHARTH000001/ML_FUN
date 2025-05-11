b3efcde49a22410dddddf58b7338f829712a38e7
Siddharth verma <79041953+SIDDHARTH000001@users.noreply.github.com>
2025-05-12 02:06:25 +0530

    Add files via upload

---
+++ Deploy HF-models On Modals/hello.py
@@ -0,0 +1,32 @@
+import modal
+from modal import App, Image
+
+# Setup
+
+app = modal.App("hello")
+image = Image.debian_slim().pip_install("requests")
+
+# Hello!
+
+import os 
+os.environ["MODAL_TOKEN_ID"]  = "Xxxxxxxxxxxxxxxxx"
+os.environ["MODAL_TOKEN_SECRET"]  = "Zzzzzzzzzzzzzzzzzz"
+
+@app.function(image=image)
+def hello() -> str:
+    import requests
+    
+    response = requests.get('https://ipinfo.io/json')
+    data = response.json()
+    city, region, country = data['city'], data['region'], data['country']
+    return f"Hello from {city}, {region}, {country}!!"
+
+
+@app.function(image=image, region="eu")
+def hello_europe() -> str:
+    import requests
+    
+    response = requests.get('https://ipinfo.io/json')
+    data = response.json()
+    city, region, country = data['city'], data['region'], data['country']
+    return f"Hello from {city}, {region}, {country}!!"
